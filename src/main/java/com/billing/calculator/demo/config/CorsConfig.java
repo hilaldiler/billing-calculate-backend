e6e55e62e62f0b54c35c313b3d58ec88e12cd8b6
youthinnco <hidiler9@gmail.com>
2024-02-10 22:33:55 +0300

    Update CorsConfig.java

--- src/main/java/com/billing/calculator/demo/config/CorsConfig.java
+++ src/main/java/com/billing/calculator/demo/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://sernteklifal.vercel.app/:8089")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowCredentials(true);
             }
