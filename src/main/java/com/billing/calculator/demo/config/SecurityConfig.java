0ce62f3123d97879b278c3ff538e778d24680ed7
youthinnco <hidiler9@gmail.com>
2024-02-18 00:44:17 +0300

    security ignored

--- src/main/java/com/billing/calculator/demo/config/SecurityConfig.java
+++ src/main/java/com/billing/calculator/demo/config/SecurityConfig.java
@@ -3,37 +3,17 @@ package com.billing.calculator.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 
 @Configuration
-public class SecurityConfig{
-	
-	@Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests().anyRequest().permitAll();
-        return http.build();
-    }
-	
+@EnableWebSecurity
+public class SecurityConfig {
 
 	@Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("OPTIONS");
-        config.addAllowedMethod("HEAD");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("DELETE");
-        config.addAllowedMethod("PATCH");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+	private void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable() // CSRF korumasını devre dışı bırak
+            .authorizeRequests()
+            .requestMatchers("/**").permitAll(); 
     }
-
 }
