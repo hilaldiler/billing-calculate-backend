b99eff5959a71a34f2dbab940e4cb1afc17a6c54
youthinnco <hidiler9@gmail.com>
2024-02-18 01:00:50 +0300

    Update DemoApplication.java

--- src/main/java/com/billing/calculator/demo/DemoApplication.java
+++ src/main/java/com/billing/calculator/demo/DemoApplication.java
@@ -5,12 +5,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, SecurityAutoConfiguration.class})
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	@Bean
+    public SecurityWebFilterChain chain(ServerHttpSecurity http, AuthenticationWebFilter webFilter) {
+        return http.authorizeExchange().anyExchange().permitAll().and()
+                .csrf().disable()
+                .build();
 	}
 
 }
