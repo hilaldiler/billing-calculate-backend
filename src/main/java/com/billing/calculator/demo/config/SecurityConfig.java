a2150497617f86f67c05bed70dc193aaf598916c
youthinnco <hidiler9@gmail.com>
2024-02-18 00:27:08 +0300

    security ignored

--- src/main/java/com/billing/calculator/demo/config/SecurityConfig.java
+++ src/main/java/com/billing/calculator/demo/config/SecurityConfig.java
@@ -2,13 +2,22 @@ package com.billing.calculator.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 
 @Configuration
-public class CorsConfig {
+public class SecurityConfig{
+	
+	@Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().anyRequest().permitAll();
+        return http.build();
+    }
+	
 
 	@Bean
     public CorsFilter corsFilter() {
@@ -26,4 +35,5 @@ public class CorsConfig {
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
+
 }
